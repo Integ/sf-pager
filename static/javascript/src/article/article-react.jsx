a1ded549c8ef185edbdc55a77e56388e0bfb14b7
Shengzhang Wen <coffeesherk@gmail.com>
2015-04-30 18:07:11 +0800

    add image view for scale

--- static/javascript/src/article/article-react.jsx
+++ static/javascript/src/article/article-react.jsx
@@ -56,4 +56,11 @@
 React.render(
     <Article />,
     document.getElementById("main-container")
-)
+);
+
+$(document).ready(function () {
+    $('article.content').on("click", "img", function () {
+        var src = $(this).attr("src");
+        window.sf.viewImage(src);
+    });
+});
