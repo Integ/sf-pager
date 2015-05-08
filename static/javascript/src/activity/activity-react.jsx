705247983b72cfacf5e6491df72c8611018e5903
Shengzhang Wen <coffeesherk@gmail.com>
2015-05-08 11:35:54 +0800

    活动 detail

--- static/javascript/src/activity/activity-react.jsx
+++ static/javascript/src/activity/activity-react.jsx
@@ -6,22 +6,33 @@
         getInitialState: function () {
             return {
                 "id": -1,
-                "name": "SegmentFault D-Day 2015 西安站",
-                "thumbUrl": "http://sfault-activity.b0.upaiyun.com/217/152/2171527794-5530d5255bb61_big",
-                "startDate": "2015-05-30 09:00 星期六",
-                "endDate": "2015-05-30 20:00 星期六",
-                "address": "杭州市 西湖大道2号 杭州红楼大酒店 5F",
-                "sponsorsList" : ['SegmentFault'],
-                "parsedText" : '2014 年 SegmentFault D-Day，5 个月，9 场技术沙龙，覆盖北京、杭州、南京上千的开发者，我们受到了越来越多的开发者的参与和肯定。这些肯定也让我们不敢减弱技术传播的速度。SegmentFault 会继续提供这样一个集思广益的窗口，让纯粹的技术沙龙，将真正热爱技术并乐于分享的开发者，聚集到一起。'
+                "name": "加载中",
+                "thumbUrl": "",
+                "startDate": "加载中",
+                "endDate": "加载中",
+                "startWeek": "加载中",
+                "endWeek": "加载中",
+                "address": "加载中",
+                "sponsorsList" : [['加载中']],
+                "parsedText" : '加载中'
             }
         },
 
         componentDidMount: function () {
             // 显示活动
-            global.showAcitity = function (data) {
+            global.showActivity = function (data) {
                 this.setState({
                     "id": data.id,
-                    "title": data.title
+                    "name": data.name,
+                    "title": data.title,
+                    "address": data.cityName,
+                    "thumbUrl": data.bannerUrl,
+                    "startDate": data.startDate,
+                    "endDate": data.endDate,
+                    "startWeek": data.startWeek,
+                    "endWeek": data.endWeek,
+                    "sponsorsList": data.sponsorsList,
+                    "parsedText": data.parsedText
                 })
             }.bind(this);
 
@@ -39,11 +50,11 @@
                             <tbody>
                                 <tr>
                                     <td width="70" className="info-label">开始时间：</td>
-                                    <td>{this.state.startDate}</td>
+                                    <td>{this.state.startDate} {this.state.startWeek}</td>
                                 </tr>
                                 <tr>
                                     <td className="info-label">结束时间：</td>
-                                    <td>{this.state.endDate}</td>
+                                    <td>{this.state.endDate} {this.state.endWeek}</td>
                                 </tr>
                                 <tr>
                                     <td className="info-label">举办地点：</td>
@@ -51,7 +62,7 @@
                                 </tr>
                                 <tr>
                                     <td className="info-label green-text">主办方：</td>
-                                    <td className="green-text">{this.state.sponsorsList[0]}</td>
+                                    <td className="green-text">{this.state.sponsorsList[0][0]}</td>
                                 </tr>
                                 <tr>
                                     <td className="info-label">报名截止：</td>
@@ -65,8 +76,8 @@
                             </tbody>
                         </table>
                     </div>
-                    <div className="activity-intro">
-                        {this.state.parsedText}
+                    <div className="activity-intro"
+                         dangerouslySetInnerHTML={{__html: this.state.parsedText }}>
                     </div>
                 </div>
             );
